c706b31d92b3a84fd77717ff3f13d9403a2beff6
mehmood-ahmad <maksuperlink@gmail.com>
2018-08-09 17:30:53 +0500

    change base component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import PomodoroClock from './components/presentational/PomodoroClock';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<PomodoroClock/>, document.getElementById('root'));
